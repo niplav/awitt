d2c8f36ef3a0ecf4a7d75941155b44a2f23dfa0b
niplav <niplav@posteo.net>
2019-06-14 18:22:15 +0200

    Worked on the better version of awitt.

--- awitt2.c
+++ awitt2.c
@@ -2,13 +2,18 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xatom.h>
 
 int main(int argc, char** argv)
 {
-	int focusstate, revert;
+	int focusstate, revert, i;
 	char* wname;
+	Status s1, s2;
 	Window* w=(Window*)calloc(1, sizeof(Window));
 	Display* dpy=XOpenDisplay(NULL);
+	XTextProperty* title=(XTextProperty*)calloc(1, sizeof(XTextProperty));
+	XClassHint* class=(XClassHint*)XAllocClassHint();
 
 	if(dpy==NULL)
 	{
@@ -19,13 +24,29 @@ int main(int argc, char** argv)
 	for(;;)
 	{
 		focusstate=XGetInputFocus(dpy, w, &revert);
+
 		printf("%d|%d, parent: %d, pointer_root: %d, none: %d\n", focusstate, revert, RevertToParent, RevertToPointerRoot, RevertToNone);
-		XFetchName(dpy, *w, &wname);
-		printf("%s\n", wname);
+
+		s1=XGetTextProperty(dpy, *w, title, XA_WM_NAME);
+		s2=XGetClassHint(dpy, *w, class);
+
+		if(revert==2)
+		{
+			/* Use XQueryTree() */
+		}
+
+		printf("s1: %d, s2: %d\n", s1, s2);
+		if(s1!=0)
+			printf("%s\n", title->value);
+		if(s2!=0)
+			printf("%s %s\n", class->res_name, class->res_class);
 		sleep(1);
 	}
 
 	XCloseDisplay(dpy);
+
+	free(title);
+	XFree(class);
 
 	return 0;
 }
