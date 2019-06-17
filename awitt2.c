7c9f019b6bd37c931517a4902b2ed6f6224835a3
niplav <niplav@posteo.net>
2019-06-17 17:18:02 +0200

    Rudimentariest solution to the parent problem.

--- awitt2.c
+++ awitt2.c
@@ -7,13 +7,18 @@
 
 int main(int argc, char** argv)
 {
-	int focusstate, revert, i;
+	int focusstate, revert, i, nchildren;
 	char* wname;
-	Status s1, s2;
-	Window* w=(Window*)calloc(1, sizeof(Window));
+	Status s1, s2, s3;
+	Window* w, * par, * root, ** children;
 	Display* dpy=XOpenDisplay(NULL);
 	XTextProperty* title=(XTextProperty*)calloc(1, sizeof(XTextProperty));
 	XClassHint* class=(XClassHint*)XAllocClassHint();
+
+	w=(Window*)calloc(1, sizeof(Window));
+	par=(Window*)calloc(1, sizeof(Window));
+	root=(Window*)calloc(1, sizeof(Window));
+	children=(Window*)calloc(16, sizeof(Window));
 
 	if(dpy==NULL)
 	{
@@ -27,13 +32,17 @@ int main(int argc, char** argv)
 
 		printf("%d|%d, parent: %d, pointer_root: %d, none: %d\n", focusstate, revert, RevertToParent, RevertToPointerRoot, RevertToNone);
 
-		s1=XGetTextProperty(dpy, *w, title, XA_WM_NAME);
-		s2=XGetClassHint(dpy, *w, class);
-
 		if(revert==2)
 		{
-			/* Use XQueryTree() */
+			/* Needless to say, this is _very hacky. */
+			/* TODO: carve this into the side of god */
+			printf("reverting to parent\n");
+			s3=XQueryTree(dpy, *w, root, par, children, &nchildren);
+			w=par;
 		}
+
+		s1=XGetTextProperty(dpy, *w, title, XA_WM_NAME);
+		s2=XGetClassHint(dpy, *w, class);
 
 		printf("s1: %d, s2: %d\n", s1, s2);
 		if(s1!=0)
@@ -47,6 +56,7 @@ int main(int argc, char** argv)
 
 	free(title);
 	XFree(class);
+	XFree(children);
 
 	return 0;
 }
